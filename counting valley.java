75ab998241aa89ad07bdaa2a472001e5cf2ccce4
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2021-12-20 13:07:55 +0300

    Create counting valley.java

---
+++ counting valley.java
@@ -0,0 +1,22 @@
+function countingValleys(n, s) {
+     const min = 2;
+     const max = 1000000;
+     s = (typeof s === "string") ? s.split('') : s;
+     // ["U", "D", "D", "D", "U", "D", "U", "U"] 
+ 
+     // check if s meets the requirements
+     if (s.length >= min
+          && s.length <= max
+          && n === parseInt(n, 0)
+          && n >= min
+          && n <= max 
+          && n === s.length) {
+          
+          // converting the array steps to integers
+          s = s.map(steps => ((steps === "U") ? 1 : -1));
+          // [1, -1, -1, -1, 1, -1, 1, 1]
+          
+          let path = 0; 
+          for(let i in s) {
+               path += s[i];
+          } 
