5b5bda19ce34c2a69e5be8d8f6cd9065cb5d4620
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2022-04-21 13:02:51 +0300

    Create nextlarge.py

---
+++ nextlarge.py
@@ -0,0 +1,26 @@
+# Definition for singly-linked list.
+# class ListNode(object):
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution(object):
+    def nextLargerNodes(self, head):
+        """
+        :type head: ListNode
+        :rtype: List[int]
+        """
+        ans = []
+        stack = []
+
+        while head:
+            while stack and head.val > ans[stack[-1]]:
+                index = stack.pop()
+                ans[index] = head.val
+            stack.append(len(ans))
+            ans.append(head.val)
+            head = head.next
+
+        for i in stack:
+        ans[i] = 0
+
+    return ans
