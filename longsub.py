552d3ad58ef0cd57c7a840a2d8cc60d2274352a3
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2022-07-08 09:34:18 +0300

    Create longsub.py

---
+++ longsub.py
@@ -0,0 +1,33 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        if not s:
+            return 0
+        output = 1
+        temp = [1]
+        tempString = ""
+        i = 0
+        j = i+1
+        tempString += s[i]
+        while i<j and j<len(s):
+            if s[i]!=s[j]:
+                if s[j] in tempString:
+                    i += 1
+                    j = i + 1
+                    temp.append(output)
+                    output = 1
+                    tempString = ""
+                    tempString += s[i]
+                else:
+                    tempString += s[j]
+                    output += 1
+                    j += 1
+                    if j==len(s):
+                        temp.append(output)
+            else:
+                i += 1
+                j = i + 1
+                temp.append(output)
+                output = 1
+                tempString = ""
+                tempString += s[i]
+        return max(temp)
