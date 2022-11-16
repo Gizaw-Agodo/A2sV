8f5683e72abad8aeed6c6b896cb8554d4efa518a
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2022-11-16 14:00:55 +0300

    Time: 86 ms (12.32%), Space: 14.2 MB (40.91%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,18 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+         
+        def generate(string, j,k):
+            if len(string) == 2*n:
+                ans.append(string)
+                return 
+            if j != 0:
+                generate(string+"(",j-1,k)
+               
+            if j<k:
+                print("r",string)
+                generate(string+")" ,j,k-1)
+              
+        ans = []
+        openP,closeP = n,n
+        generate("",openP,closeP)
+        return ans
