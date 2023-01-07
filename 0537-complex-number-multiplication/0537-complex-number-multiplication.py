c9f672e027a78988b1b91c592744428e61d4755a
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-01-07 19:49:26 +0300

    Time: 40 ms (58.80%), Space: 13.8 MB (68.54%) - LeetHub

---
+++ 0537-complex-number-multiplication/0537-complex-number-multiplication.py
@@ -0,0 +1,7 @@
+class Solution:
+    def complexNumberMultiply(self, num1: str, num2: str) -> str:
+        num1 = num1.split("+")
+        num2 = num2.split("+")
+        real = int(num1[0])*int(num2[0]) + -1*(int(num1[1][:-1])*int(num2[1][:-1]))
+        imaginary = int(num1[0])*int(num2[1][:-1]) + int(num2[0])*int(num1[1][:-1])
+        return str(real)+ "+" + str(imaginary)+"i"
