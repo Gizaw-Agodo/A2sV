1d78a2b0fb3f0dcfc2635e11e3e985d867d353cf
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-02-10 21:36:33 +0300

    Time: 55 ms (93.21%), Space: 13.9 MB (96.99%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.py
@@ -0,0 +1,36 @@
+class Solution:
+    def compress(self, chars: List[str]) -> int:
+        chars.append("-")
+        n = len(chars)
+        slow = 0
+        fast = 0
+        count = 1
+        
+        for fast in range(n):
+            if fast == 0 :
+                continue
+           
+            if chars[fast] == chars[fast-1] :
+                count+=1
+            else:
+                if count == 1:
+                    chars[slow] = chars[fast-1]
+                    slow+=1
+                else:
+                    chars[slow] = chars[fast-1]
+                    slow+=1
+                    if count<10:
+                        chars[slow] = str(count)
+                        slow+=1
+                    else:
+                        charnums = list(str(count))
+                        for k in charnums:
+                            chars[slow] = k
+                            slow+=1
+                count = 1
+            
+        for i in range(n-slow):
+            chars.pop()
+                
+                
+        return len(chars)
