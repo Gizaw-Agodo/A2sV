c0e583d374462f4049a05820bf34a3a360d74954
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-03-01 12:52:30 +0300

    Time: 32 ms (67.99%), Space: 13.8 MB (56.28%) - LeetHub

--- 0050-powx-n/0050-powx-n.py
+++ 0050-powx-n/0050-powx-n.py
@@ -1,18 +1,18 @@
 class Solution:
     def myPow(self, x: float, n: int) -> float:
-        if x==0 or x==1 or n==1:
-            return x 
-
-        if x==-1:
-            if n%2 ==0:
-                return 1
-            else:
-                return -1
-        if n==0:
+        answer = self.findPower(x,abs(n))
+        if n < 0:
+            return 1/answer
+        return answer
+        
+    def findPower(self, x, n):
+        # base case
+        if n == 0 :
             return 1
-        if n<0:
-            return 1/self.myPow(x,-n)
-        val = self.myPow(x,n//2)
-        if n%2 ==0:
-            return val*val
-        return val*val*x
+        
+        if n % 2 == 0:
+            val = self.findPower(x,n//2)
+            return val * val
+        return x * self.findPower(x, n-1)
+        
+    
