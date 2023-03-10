ca55f37646b44efe8b212c2aeda50ce0e22e4875
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-03-10 18:17:13 +0300

    Time: 829 ms (18.57%), Space: 85.9 MB (6.26%) - LeetHub

--- 0148-sort-list/0148-sort-list.py
+++ 0148-sort-list/0148-sort-list.py
@@ -3,19 +3,52 @@
 #     def __init__(self, val=0, next=None):
 #         self.val = val
 #         self.next = next
+
 class Solution:
+    def mergeTwoLists(self, list1, list2):
+        if list1 == None :
+            return list2
+        elif list2 == None:
+            return list1
+        if list1.val <= list2.val :
+            list1.next = self.mergeTwoLists(list1.next, list2)
+            return list1
+        else:
+            list2.next = self.mergeTwoLists(list1, list2.next)
+            return list2
+    
+    def divide(self, node):
+        slow = node
+        fast = node.next
+        while fast and fast.next:
+            slow = slow.next
+            fast = fast.next.next
+        temp = slow.next
+        slow.next = None
+        return temp
+    
     def sortList(self, head: Optional[ListNode]) -> Optional[ListNode]:
-        temp_list = []
-        while head:
-            temp_list.append(head.val)
-            head = head.next
         
-        temp_list.sort()
-        dummy_node = ListNode(0)
-        temp = dummy_node
-        for val in temp_list:
-            new_node = ListNode(val)
-            temp.next = new_node
-            temp = new_node
+        if head == None or head.next == None:
+            return head
+        slow = self.divide(head)
+        list1 = self.sortList(head)
+        list2 = self.sortList(slow)
+        return(self.mergeTwoLists(list1, list2))
+         
+        
             
-        return dummy_node.next
+        
+        
+        
+        
+        
+        
+        
+     
+    
+    
+    
+    
+    
+                
