7cef2ecfcc99c02f7142e9ec5f7879920304a2c0
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-03-23 18:45:52 +0300

    Time: 152 ms (32.18%), Space: 15.2 MB (67.42%) - LeetHub

--- 0268-missing-number/0268-missing-number.py
+++ 0268-missing-number/0268-missing-number.py
@@ -1,7 +1,14 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
-        nums = set(nums)
-        
-        for i in range(len(nums) +1):
-            if i not in nums :
-                return i
+        def cyclicSort(arr):
+            for  i in range(len(arr)):
+                while arr[i] != i  and arr[i] != -1:
+                    arr[arr[i]],arr[i] = arr[i],arr[arr[i]]
+            
+            for i in range(len(arr)):
+                if arr[i] == -1:
+                    return i
+        nums.append(-1)
+        ans = cyclicSort(nums)
+        print(ans)
+        return ans
