51ae2c84dbf6c74449f5ab1bc586f5001386cbfb
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-03-26 23:22:01 +0300

    Time: 3315 ms (57.70%), Space: 42.6 MB (22.65%) - LeetHub

--- 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
+++ 0315-count-of-smaller-numbers-after-self/0315-count-of-smaller-numbers-after-self.py
@@ -4,11 +4,11 @@ class Solution:
         nums = [(nums[i],i) for i in range(len(nums))]
         
         def merge(left_half, right_half):
-            count = 0
-            p1 = 0
-            p2 = 0
+            count = p1 = p2 = 0
             sorted_arr = []
+            
             while p1 < len(left_half) and p2 < len(right_half):
+                
                 if right_half[p2][0] < left_half[p1][0]:
                     count += 1
                     sorted_arr.append(right_half[p2])
@@ -31,6 +31,7 @@ class Solution:
         def mergeSort(left, right, arr):
             if left == right:
                 return [arr[left]]
+            
             mid = left + (right - left)//2
             left_half = mergeSort(left, mid, arr)
             right_half = mergeSort(mid + 1, right, arr)
