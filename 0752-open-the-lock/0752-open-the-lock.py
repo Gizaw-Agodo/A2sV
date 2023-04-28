131ef79584d3abcedf8418f2f34f1340182edb50
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-04-28 19:25:07 +0300

    Time: 571 ms (82.52%), Space: 17.9 MB (5.12%) - LeetHub

--- 0752-open-the-lock/0752-open-the-lock.py
+++ 0752-open-the-lock/0752-open-the-lock.py
@@ -5,7 +5,7 @@ class Solution:
         queue = deque([["0","0","0","0"]])
         visited = set(deadends)
         visited.add('0000')
-        directions = [-1, 1]
+      
         
         if '0000' in deadends:
             return -1
@@ -17,15 +17,16 @@ class Solution:
             while queue:
                 
                 length = len(queue)
+                
                 for k in range(length):
                     string = queue.popleft()
+                    
                     for i in range(4):
-                        for direction in directions :
+                        for direction in [1, -1] :
                             tempstring = string.copy()
                             tempstring[i] = str((int(string[i]) + direction)% 10) 
-                            if tempstring[i] == "-1":
-                                tempstring[i] = '9'
                             curstr = "".join(tempstring)
+                            
                             if curstr == target:
                                 return level
                             if  curstr not in visited:
@@ -34,9 +35,7 @@ class Solution:
                 level += 1
             return -1
         
-        if target == "0000" or target in deadends:
-            return 0
-        return bfs(target)
+        return 0 if  target == "0000" else bfs(target)
                 
            
                 
