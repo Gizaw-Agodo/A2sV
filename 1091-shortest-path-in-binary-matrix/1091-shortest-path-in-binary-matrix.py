deff8f7133078ce94c83a66a7ae717d51f456e84
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-04-30 21:15:46 +0300

    Time: 600 ms (67.95%), Space: 16.7 MB (7.85%) - LeetHub

--- 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
+++ 1091-shortest-path-in-binary-matrix/1091-shortest-path-in-binary-matrix.py
@@ -1,43 +1,31 @@
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
-        m = len(grid[0])
+        # time O(M * N)
+        # space O(M * N)
+        
         n = len(grid)
-        if grid[0][0] == 1:
-            return -1
-        
-        visited = set([(0,0)])
-        queue = deque([(0,0)])
-        
-        shortd = [[float('inf') for i in range(m)] for _ in range(n)]
         
         directions = [(1,0),(0,1),(-1,0),(0,-1),(1,1),(-1,-1),(1,-1),(-1,1)]
-        shortd[0][0] = 1
         
-        def isBounded (row, col, grid):
-            return 0 <= row < len(grid) and 0 <= col < len(grid[0])
+        def isBounded (row, col, n):
+            return 0 <= row < n and 0 <= col < n
+        
+        # check for 1 at start and end 
+        if grid[0][0] or grid[n-1][n-1]:
+            return -1
+        
+        queue = deque([(0, 0, 1)])
+        
+        def bfs ():
             
-        def bfs(grid):
-            distance = 1
             while queue:
-               
-                length = len(queue)
-                
-                for i in range(length):
-                    
-                    i,j = queue.popleft()
-                    for row,col in directions : 
-                        nrow = row + i
-                        ncol = col + j
-
-                        if isBounded(nrow, ncol, grid) and (nrow,ncol) not in visited:
-                            if grid[nrow][ncol] == 0:
-                                shortd[nrow][ncol] = min(shortd[nrow][ncol],distance + 1)
-                                visited.add((nrow, ncol))
-                                queue.append((nrow,ncol))
-
-                distance += 1
-        bfs(grid)
-        
-        print(shortd)
-        return shortd[m-1][n-1] if shortd[m-1][n-1] != float('inf') else -1
-                            
+                r, c, d = queue.popleft()
+                if r == n-1 and c == n-1 :
+                    return d
+                for row, col in directions :
+                    nrow, ncol = r + row, col + c
+                    if isBounded(nrow, ncol, n) and not grid[nrow][ncol]:
+                        queue.append((nrow, ncol, d + 1))
+                        grid[nrow][ncol] = 1
+            return -1
+        return bfs()
