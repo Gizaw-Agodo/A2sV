6ea22eb58669539469088c2a1e8ec76b46b1faa9
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-05-03 19:36:25 +0300

    Time: 546 ms (52.19%), Space: 28.8 MB (29.31%) - LeetHub

--- 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
+++ 0215-kth-largest-element-in-an-array/0215-kth-largest-element-in-an-array.py
@@ -1,25 +1,6 @@
 class Solution:
     def findKthLargest(self, nums: List[int], k: int) -> int:
-        def queckSort(start, end):
-            if end == start:
-                return None
-            pivot = nums[start]
-            read = start + 1
-            write = start + 1
-            while read < end:
-                if nums[read] < pivot:
-                    nums[read],nums[write] = nums[write],nums[read]
-                    write += 1
-                read += 1
-            nums[start],nums[write-1] = nums[write-1],nums[start]
-            if write-1 == len(nums) - k:
-                return nums[write-1]
-            left_ans = queckSort(start,write-1)
-            if left_ans == None:
-                right_ans = queckSort(write,end)
-                return right_ans
-            return left_ans
-
-        answer = queckSort(0,len(nums))
-        return answer if answer != None else nums[-k]
-    
+        heapify(nums)
+        for i in range(len(nums) -k , 0, -1):
+            heappop(nums)
+        return heappop(nums)
