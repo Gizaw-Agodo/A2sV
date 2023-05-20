8988ceef47aedf24283c4e8fcce733a183c3359c
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-05-20 19:56:17 +0300

    Time: 178 ms (48.19%), Space: 17.2 MB (22.28%) - LeetHub

---
+++ 0947-most-stones-removed-with-same-row-or-column/0947-most-stones-removed-with-same-row-or-column.py
@@ -0,0 +1,41 @@
+class Solution:
+    def removeStones(self, stones: List[List[int]]) -> int:
+        size =  {}
+        parent = {}
+        
+        for x,y in stones:
+            x, y = f'x{x}',f'y{y}'
+            size[x], size[y] = 1, 1
+            parent[x], parent[y] = x, y
+            
+        
+        def find(x):
+            if x != parent[x]:
+                parent[x] = find(parent[x])
+            return parent[x]
+                 
+        def union(x, y):
+            xroot = find(x)
+            yroot = find(y)
+            
+            if size[xroot] >= size[yroot]:
+                parent[yroot] = xroot
+                size[xroot] += size[yroot]
+            else:
+                parent[xroot] = yroot
+                size[yroot] += size[xroot]
+       
+        for x, y in stones:
+            union(f'x{x}', f'y{y}')
+        
+        return len(stones) - sum(parent[element] == element for element in parent)
+
+            
+            
+                
+        
+            
+                
+            
+        
+        
