2ed45d7ac474a6f4da229fe8a6ccc1e954a1241a
Gizaw Agodo <91599349+Gizaw-Agodo@users.noreply.github.com>
2023-10-17 18:15:09 +0300

    Update smallest-range-i.py

--- Leetcode/smallest-range-i.py
+++ Leetcode/smallest-range-i.py
@@ -2,7 +2,5 @@ class Solution:
     def smallestRangeI(self, nums: List[int], k: int) -> int:
         nums.sort()
         val =  nums[-1] - nums[0] - 2*k
-        if val < 0:
-            return 0
-        return val
+        return max(0,val)
         
